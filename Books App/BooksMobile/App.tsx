02109aff11a24fb89ff49c719ac8844914a82383
timothymoniaga <tmon0012@student.monash.edu>
2023-02-07 20:41:47 +1100

    apollo client added in
    apollo client in app.tsx

--- Books App/BooksMobile/App.tsx
+++ Books App/BooksMobile/App.tsx
@@ -1,9 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+const API_KEY = "gaesti::stepzen.net+1000::e62f8dacf34e977ae22e531df6b9e876ceb6e1c7ee1c0a60d3d600c3ed0f6123";
+
+const client = new ApolloClient({
+  uri: "https://gaesti.stepzen.net/api/reeling-opossum/__graphqll",
+  headers: {
+    Authorization: `Apikey ${API_KEY}`
+  },
+  cache: new InMemoryCache(),
+
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,7 +26,9 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <ApolloProvider client={client}>
+          <Navigation colorScheme={colorScheme} />
+        </ApolloProvider>
         <StatusBar />
       </SafeAreaProvider>
     );
